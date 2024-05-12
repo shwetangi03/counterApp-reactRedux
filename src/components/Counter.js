59652aea83e5a27a6943a9313a301f99eda8e82c
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-05-12 23:48:33 +0530

    Auth Reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,11 @@
+import { useSelector, useDispatch } from "react-redux";
 import { counterActions } from "../store/redux";
 import classes from "./Counter.module.css";
-import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
     dispatch(counterActions.increment());
